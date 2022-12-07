c29be51776a0d034beb77314c1a0139b16c8153d
Allan Luo <allanluo@gmail.com>
2022-12-07 14:21:13 -0600

    added invite restrictions

--- Components/InvitePeople.js
+++ Components/InvitePeople.js
@@ -1,43 +1,42 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import Modal from '@mui/material/Modal';
-import CloseIcon from '@mui/icons-material/Close';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { collection, query, where, getDocs, addDoc } from 'firebase/firestore';
-import { db } from '../firebase';
-import Alert from '@mui/material/Alert';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Modal from "@mui/material/Modal";
+import CloseIcon from "@mui/icons-material/Close";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { collection, query, where, getDocs, addDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import Alert from "@mui/material/Alert";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 600,
   height: 300,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
   borderRadius: 5,
-  alignItems: 'center',
+  alignItems: "center",
 };
 
 const InvitePeople = () => {
   const [open, setOpen] = useState(false);
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [saved, setSave] = React.useState(false);
   const [error, setError] = React.useState(false);
-  const { singleEcosystem, singleEcosystemTasks } = useSelector(
-    (state) => state
-  );
+  const { singleEcosystem, singleEcosystemTasks, ecosystemMembers } =
+    useSelector((state) => state);
 
   const handleOpen = () => {
     setOpen(true);
@@ -57,19 +56,32 @@ const InvitePeople = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // let emailFound = '';
-    const q = query(collection(db, 'Users'), where('email', '==', email));
-    console.log('>>>', q);
+    // find the invitee to see if they exist in Users
+    const q = query(collection(db, "Users"), where("email", "==", email));
+    console.log(">>>", q);
     const querySnapshot = await getDocs(q);
-
     if (querySnapshot.size === 1) {
-      await addDoc(collection(db, 'Invites'), {
-        ecosystemId: singleEcosystem.id,
-        orgName: singleEcosystem.orgName,
-        userId: querySnapshot.docs[0].id,
-        pending: true,
-      });
-      setSave(!saved);
+      // check if invitee is already invited
+      const x = query(
+        collection(db, "Invites"),
+        where("userId", "==", querySnapshot.docs[0].id),
+        where("ecosystemId", "==", singleEcosystem.id)
+      );
+      const inviteSnapshot = await getDocs(x);
+      //check if invitee is already in the group
+      let alreadyHere = ecosystemMembers.find(
+        (member) => member.userId === querySnapshot.docs[0].id
+      );
+
+      if (!alreadyHere && inviteSnapshot.size === 0) {
+        await addDoc(collection(db, "Invites"), {
+          ecosystemId: singleEcosystem.id,
+          orgName: singleEcosystem.orgName,
+          userId: querySnapshot.docs[0].id,
+          pending: true,
+        });
+        setSave(!saved);
+      }
     } else {
       setError(!error);
     }
@@ -83,9 +95,9 @@ const InvitePeople = () => {
   };
 
   return (
-    <div className='flex justify-center'>
+    <div className="flex justify-center">
       <button
-        className='bg-blue-300 hover:bg-blue-400 text-black px-4 py-2 rounded-2xl h-10 m-2 w-1/2'
+        className="bg-blue-300 hover:bg-blue-400 text-black px-4 py-2 rounded-2xl h-10 m-2 w-1/2"
         onClick={handleOpen}
       >
         Invite
@@ -94,32 +106,32 @@ const InvitePeople = () => {
         <DialogTitle>
           Invite Someone
           <CloseIcon
-            className='absolute top-0 right-0 m-3 duration-300 hover:scale-110 hover:font-bold'
+            className="absolute top-0 right-0 m-3 duration-300 hover:scale-110 hover:font-bold"
             onClick={handleClose}
           />
         </DialogTitle>
         <DialogContent>
-          <DialogContentText className='w-screen'>
-            Enter Email Address:{' '}
+          <DialogContentText className="w-screen">
+            Enter Email Address:{" "}
           </DialogContentText>
           <TextField
             autoFocus
-            margin='dense'
-            id='name'
-            label='Email Address'
-            type='email'
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
             fullWidth
-            variant='standard'
-            name='email'
+            variant="standard"
+            name="email"
             onChange={handleChange}
-            className='w-screen'
+            className="w-screen"
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleSubmit}>
-            {saved ? <Alert severity='success'>Invited</Alert> : 'Invite'}
+            {saved ? <Alert severity="success">Invited</Alert> : "Invite"}
           </Button>
-          {error && <Alert severity='error'>User not found</Alert>}
+          {error && <Alert severity="error">User not found</Alert>}
         </DialogActions>
       </Dialog>
       {/* <Modal
